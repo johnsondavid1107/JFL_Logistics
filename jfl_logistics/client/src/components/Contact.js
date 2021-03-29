148d350fb079b6f982e1c1b1e7af1027a7ba285f
Ness <johnsondavid1107@gmail.com>
2021-03-29 09:38:22 -0400

    added css to contact form and set the reset for all inputs on submit.  With working toasts

--- jfl_logistics/client/src/components/Contact.js
+++ jfl_logistics/client/src/components/Contact.js
@@ -2,15 +2,18 @@ import { React, useState } from "react"
 import emailjs from "emailjs-com"
 import { init } from "emailjs-com"
 import { useToasts } from "react-toast-notifications"
+import "./CSS/Contact.css"
 
 function Contact() {
 
     init("user_Dd7t64AVJTOGd0UtzbGU2")
-    const [name, setName] = useState("");
+    const [fName, setfName] = useState("");
+    const [lName, setlName] = useState("");
     const [email, setEmail] = useState("");
     const [number, setNumber] = useState("");
     const [message, setMessage] = useState("");
     const [radio, setRadio] = useState("");
+    const [radioClick, setRadioClick] = useState()
     const { addToast } = useToasts()
 
     const styles = {
@@ -23,9 +26,7 @@ function Contact() {
         }
     }
 
-    function sendMessage() {
-
-
+    function sendMessage(event) {
         const templateParams = {
             from_name: email,
             to_name: "johnsondavid1107@gmail.com",
@@ -33,7 +34,7 @@ function Contact() {
             radio: radio,
             phone_number: number
         };
-        emailjs.send("contact_service", "contact_form", templateParams)
+        emailjs.send("contact_service", "contactform", templateParams)
             .then(function (response) {
                 console.log("message sent")
                 console.log(response)
@@ -43,40 +44,74 @@ function Contact() {
                 addToast("Unknown Error.. Please try again", { appearance: "error", autoDismiss: true })
             })
 
-            
+        setlName(""); setfName(""); setEmail(""); setNumber(""); setMessage(""); setRadioClick(false);
+
 
     }
 
-
-
     function handleClick(e) {
+        //need a preventDevault to prevent the page from reloading.  We need the toast to display
         e.preventDefault()
         sendMessage()
+
     }
 
     function handleEmailChange(event) {
+        setRadioClick(undefined)
         const { value } = event.target
         setEmail(value)
         console.log(value)
 
     }
     function handleMessageChange(event) {
+        setRadioClick(undefined)
         const { value } = event.target
         setMessage(value)
         console.log(value)
     }
     function handleNumberChange(event) {
+        setRadioClick(undefined)
         const { value } = event.target
         setNumber(value)
         console.log(value)
     }
+    function handlefNameChange(event) {
+        setRadioClick(undefined)
+        const { value } = event.target
+        setfName(value)
+        console.log(value)
+    }
+    function handlelNameChange(event) {
+        setRadioClick(undefined)
+        const { value } = event.target
+        setlName(value)
+        console.log(value)
+    }
     function handleFormClick(event) {
+        if (radioClick === false) {
+            setRadioClick(undefined)
+        }
+
         const { name } = event.target
 
         setRadio(name)
         console.log(name)
 
+
+
+
+
     }
+    function changeRadio(e) {
+
+
+        console.log(e.target.checked)
+        console.log(radioClick)
+
+    }
+
+
+
 
     return (
         <div className="container-fluid" style={styles.sides}>
@@ -99,13 +134,13 @@ function Contact() {
 
 
                                                 <label> First Name</label>
-                                                <input type="text" className="form-control"></input>
+                                                <input type="text" className="form-control" onChange={handlefNameChange} value={fName}></input>
                                             </div>
                                             <div className="col-6">
 
 
                                                 <label> Last Name</label>
-                                                <input type="text" className="form-control"></input>
+                                                <input type="text" className="form-control" onChange={handlelNameChange} value={lName}></input>
                                             </div>
 
 
@@ -118,43 +153,61 @@ function Contact() {
 
                                             <div className="col-6">
                                                 <label> Email Address</label>
-                                                <input type="email" className="form-control" name={email} onChange={handleEmailChange}></input>
+                                                <input type="email" className="form-control" name={email} value={email} onChange={handleEmailChange}></input>
 
                                             </div>
 
                                             <div className="col-6">
                                                 <label> Phone Number</label>
-                                                <input type="text" className="form-control" name={number} onChange={handleNumberChange}></input>
+                                                <input type="text" className="form-control" name={number} value={number} onChange={handleNumberChange}></input>
 
                                             </div>
                                         </div>
 
                                     </div>
 
+
                                     <div className="form-check">
-                                        <input className="form-check-input" type="checkbox" value="" onClick={handleFormClick} name="Warehouseing" />
+                                        <input className="form-check-input" type="checkbox"
+                                            checked={radioClick}
+                                            onClick={handleFormClick}
+                                            onChange={changeRadio}
+                                            name="Warehouseing" />
                                         <label className="form-check-label">
                                             Warehousing
-                                        </label>
+                                                </label>
                                     </div>
                                     <div className="form-check">
-                                        <input className="form-check-input" type="checkbox" value="" onClick={handleFormClick} name="Transportation" />
+                                        <input className="form-check-input" type="checkbox"
+                                            checked={radioClick}
+                                            onClick={handleFormClick}
+                                            onChange={changeRadio}
+                                            name="Transportation" />
                                         <label className="form-check-label">
                                             Transportation
-                                        </label>
+                                                </label>
                                     </div>
                                     <div className="form-check">
-                                        <input className="form-check-input" type="checkbox" value="" onClick={handleFormClick} name="General Information" />
-                                        <label className="form-check-label">
-                                            General Information
-                                        </label>
-                                    </div>
-                                    <div className="form-check">
-                                        <input className="form-check-input" type="checkbox" value="" onClick={handleFormClick} name="defaultCheck1" />
+                                        <input className="form-check-input" type="checkbox"
+                                            checked={radioClick}
+                                            onClick={handleFormClick}
+                                            onChange={changeRadio}
+                                            name="Distribution" />
                                         <label className="form-check-label">
                                             Distrubution
-                                        </label>
+                                                </label>
                                     </div>
+                                    <div className="form-check">
+                                        <input className="form-check-input" type="checkbox"
+                                            checked={radioClick}
+                                            onClick={handleFormClick}
+                                            onChange={changeRadio}
+                                            name="General Information" />
+                                        <label className="form-check-label">
+                                            General Information
+                                                </label>
+                                    </div>
+
 
 
 
