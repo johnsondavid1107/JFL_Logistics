db33cbd671e86d0a3680ccaefafc7c40eea32e4a
Ness <johnsondavid1107@gmail.com>
2021-03-30 17:09:51 -0400

    changed styling for the position of the socialmedia bar

--- jfl_logistics/client/src/components/SocialMediaFooter.js
+++ jfl_logistics/client/src/components/SocialMediaFooter.js
@@ -14,67 +14,77 @@ function SocialMediaFooter() {
     const styles = {
         noPadding: {
             padding: 0,
-            marginTop: "0px"
-
-        },
-        color: {
+            marginTop: "0px",
             backgroundColor: "#cc1d00",
             paddingTop: "10px",
             paddingBottom: "10px"
-        }
+
+        },
+
     }
     return (
         <div className="container-fluid" style={styles.noPadding}>
-            <footer className="footer">
 
-                <div className="container-fluid" style={styles.color}>
+            <div className="row d-flex justify-content-center">
+                <footer className="footer">
 
-                    <div className="row d-flex justify-content-center">
-                        <div className="col-4"></div>
-                        <div className="col-7">
+                    <div className="col-12 d-flex flex-column ">
+                        <div>
                             <a href="https://www.linkedin.com/in/mike-lapi-0794bb36/" target="_blank">
                                 <AiFillLinkedin className={"iconFooter"} size={"2em"} />
                             </a>
                             <b className="iconText">JFL LinkedIn</b>
                         </div>
-                        <div className="col-4"></div>
-                        <div className="col-7">
+
+                        <div>
                             <AiFillFacebook className={"iconFooter"} size={"2em"} />
                             <b className="iconText">JFL Facebook</b>
                         </div>
 
-                    </div>
 
-                    <div className="row d-flex justify-content-center">
-                        <div className="col-4"></div>
-
-                        <div className="col-7">
+                        <div>
                             <a href="https://www.google.com/maps/place/178+3rd+Ave,+Paterson,+NJ+07514/@40.9364382,-74.145322,17z/data=!3m1!4b1!4m5!3m4!1s0x89c2fc8a6b98371d:0x9b161454cf375a5a!8m2!3d40.9364382!4d-74.1431333" target="_blank">
                                 <GiPositionMarker className={"iconFooter"} size={"2em"} />
                             </a>
-                            <a href="https://www.google.com/maps/place/178+3rd+Ave,+Paterson,+NJ+07514/@40.9364382,-74.145322,17z/data=!3m1!4b1!4m5!3m4!1s0x89c2fc8a6b98371d:0x9b161454cf375a5a!8m2!3d40.9364382!4d-74.1431333" target="_blank">
+
+
+
+                            <a
+                                href="https://www.google.com/maps/place/178+3rd+Ave,+Paterson,+NJ+07514/@40.9364382,-74.145322,17z/data=!3m1!4b1!4m5!3m4!1s0x89c2fc8a6b98371d:0x9b161454cf375a5a!8m2!3d40.9364382!4d-74.1431333"
+                                target="_blank">
                                 <b className="iconText">178-184 3rd Ave, Paterson, NJ 07514</b>
                             </a>
                         </div>
-                    </div>
-                    <div className="row d-flex justify-content-center">
-                        <div className="col-4"></div>
-                        <div className="col-7">
-                            <a href="mailto:alpinemike902@aol.com"> <AiFillMail className={"iconFooter"} size={"2em"} /></a>
-                            <a href="mailto:alpinemike902@aol.com"><b className="iconText"> alpinemike902@aol.com</b></a>
 
+                        <div>
+                            <a
+                                href="mailto:alpinemike902@aol.com">
+                                <AiFillMail
+                                    className={"iconFooter"}
+                                    size={"2em"} /></a>
+                            <a
+                                href="mailto:alpinemike902@aol.com">
+                                <b className="iconText"> alpinemike902@aol.com
+                         </b>
+                            </a>
                         </div>
                     </div>
 
+                </footer>
+            </div>
 
 
 
-                </div>
 
 
-            </footer>
 
-        </div>
+
+
+
+
+
+
+        </div >
     )
 }
 export default SocialMediaFooter
